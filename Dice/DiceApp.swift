7462ddb7fdc9220b7ce34e2cc815379d41e73e26
Kawarai-S <kayawakakururu@icloud.com>
2023-08-11 17:11:16 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by 瓦井つばさ on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
